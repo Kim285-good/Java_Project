f25b978be02e3ade437365c598563a37c2aedce3
Dohwan Kim <bar0805@naver.com>
2024-05-14 10:48:29 +0900

    dd

---
+++ src/main/java/com/example/springbasic/member/controller/Homecontroller.java
@@ -0,0 +1,12 @@
+package com.example.springbasic.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Homecontroller {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
